4e9c1bf63d85fe50989220a530e8b7fb79404685
Faruk Furkan Özduran <ffurkan1999@gmail.com>
2024-08-13 11:48:59 +0300

    Add project files.

---
+++ DapperDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DapperDemo.Models;
+
+namespace DapperDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Company>().Ignore(t => t.Employees);
+
+            modelBuilder.Entity<Employee>().HasOne(c => c.Company).WithMany(e => e.Employees).HasForeignKey(e => e.CompanyId);
+        }
+    }
+}
---
+++ DapperDemo/Program.cs
@@ -0,0 +1,49 @@
+using DapperDemo.Data;
+using DapperDemo.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace DapperDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var connectionString = "DefaultConnection";
+            builder.Services.AddDbContext<ApplicationDbContext>
+                (options => options.UseSqlServer
+                (builder.Configuration.GetConnectionString(connectionString))
+                );
+            builder.Services.AddScoped<ICompanyRepository, CompanyRepositorySP>();
+            builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
